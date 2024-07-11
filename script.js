d3eb53f88fedea8af1d7336ddc7bde654a74b734
sosi <79177863+cyber-sosi@users.noreply.github.com>
2024-07-11 18:32:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /* .js files add interaction to your website */
 var factList = [
-  "fact1",
-  "fact2",
-  "fact3",
-  "fact4"
+  "Global warming is causing the Earth’s average surface temperature to rise which, in turn, is causing changes in our natural climate systems. These changes are making all sorts of extreme weather events more likely and more severe, including more intense droughts, heatwaves and hurricanes but also, strangely, an increased potential for more severe cold weather events.",
+  "The most abundant greenhouse gas, accounting for about two-thirds of greenhouse gases, carbon dioxide (CO2), is largely the product of burning fossil fuels.",
+  "Methane, the primary component of natural gas, is responsible for more than 25 per cent of the warming we are experiencing today. It is a powerful pollutant with a global warming potential over 80 times greater than CO2 during the 20 years after it is released into the atmosphere.",
+  "30% of the world’s population is exposed to deadly heat waves more than 20 days a year.",
+  "Based on today’s insufficient global commitments to reduce climate polluting emissions, a rebound in greenhouse gases from a return to high-carbon societies after the pandemic may push 2030 emissions even higher – up to 60 GtCO2e.",
+  "Global temperatures are now at their highest since records began. In fact, the 10 warmest years on Earth, since 1880, have occurred since 2014."
 ];
 
 var fact = document.getElementById("fact");
